3f08ec5de3bbae2051cd824611ee614f745631d8
MangeshBhosale <66949442+MangeshBhosale@users.noreply.github.com>
2021-08-17 11:56:18 +0530

    Add files via upload

---
+++ twiter.js
@@ -0,0 +1,14 @@
+function count(obj){
+    // var maxLength = 20;
+    var strLength = obj.value.length;
+    console.log(strLength);
+    document.getElementById("charNum").innerHTML = strLength;
+
+}
+
+
+
+
+
+
+
